77fc03b8ccb5c9ea8e3e2ed1ce916d377285fc6d
Jan Lindström <janmilinds@users.noreply.github.com>
2026-03-26 18:26:58 +0200

    feat: add read-only temperature sensors #5

---
+++ custom_components/airfi/sensor/__init__.py
@@ -0,0 +1,32 @@
+"""Sensor platform for airfi."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from custom_components.airfi.const import PARALLEL_UPDATES as PARALLEL_UPDATES
+from homeassistant.components.sensor import SensorEntityDescription
+
+from .temperature import ENTITY_DESCRIPTIONS as TEMPERATURE_DESCRIPTIONS, AirfiTemperatureSensor
+
+if TYPE_CHECKING:
+    from custom_components.airfi.data import AirfiConfigEntry
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+ENTITY_DESCRIPTIONS: tuple[SensorEntityDescription, ...] = (*TEMPERATURE_DESCRIPTIONS,)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: AirfiConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the sensor platform."""
+    async_add_entities(
+        AirfiTemperatureSensor(
+            coordinator=entry.runtime_data.coordinator,
+            entity_description=entity_description,
+        )
+        for entity_description in TEMPERATURE_DESCRIPTIONS
+    )
